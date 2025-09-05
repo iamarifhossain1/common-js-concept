bc4b9eb3a44c9cd755c4df65b95a979ab97bcaec
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-05 16:19:52 +0600

    data-type

---
+++ data-type.js
@@ -0,0 +1,22 @@
+// JS is a dynamic typed programming language
+
+let data = "Tormuj";
+data = false;
+data = { name: "Arif Hossain" };
+data = [445];
+data = undefined;
+data = null;
+console.log(typeof data);
+
+// JavaScript primitive data type
+
+const price = 50;
+const name = "Arif Hossain";
+let isKnown = undefined; // usually you should not set value to undefined
+let isLocal;
+const merit = null;
+
+// JavaScript non primitive data type
+
+const student = { name: "Arif Hossain", location: "Mirpur", subject: "BBA" };
+const friends = ["Arif", "Hanif", "Mousumi"];
