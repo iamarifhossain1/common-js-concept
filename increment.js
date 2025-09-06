9c150808cdfe849d3fb9fc3bf73cf6e944f7b4c1
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-06 18:49:30 +0600

    increment

---
+++ increment.js
@@ -0,0 +1,7 @@
+let counter = 0;
+// const count = counter + 1;
+// counter = +1;
+console.log((counter = counter + 1));
+console.log((counter += 1));
+console.log(counter++);
+console.log(++counter);
